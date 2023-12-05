100f07af26f7d63c86fbd613fb29b2cc943d717b
Ashley Ngo <106159874+ashsaaa@users.noreply.github.com>
2023-12-06 01:21:14 +0800

    Added a Add Reservation Function

--- HotelReservation_Ngo/reserve.cs
+++ HotelReservation_Ngo/reserve.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,46 @@ namespace HotelReservation_Ngo
 {
     public partial class reserve : Form
     {
+        MySqlConnection connection = new MySqlConnection("server=localhost;user=root;password=;database=celestia;");
+
         public reserve()
         {
             InitializeComponent();
+            DisplayReservations();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            addReserve addReserve = new addReserve();
+            addReserve.Show();
+        }
+
+        private void DisplayReservations()
+        {
+            string query = @"SELECT reserve.id, CONCAT(client.c_lname, ', ', client.c_fname) AS ClientName, rdetails.rname AS RoomName, reserve.chIn AS CheckInDate, reserve.chOut AS CheckOutDate, reserve.Confirmation
+                             FROM reserve
+                             INNER JOIN client ON reserve.clientID = client.cid
+                             INNER JOIN rdetails ON reserve.roomID = rdetails.rid";
+
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                System.Data.DataTable dt = new System.Data.DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
     }
 }
