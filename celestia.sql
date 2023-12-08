df0d76b745b1181169d84c494d28d38a082190e9
seollemi <114889690+seollemi@users.noreply.github.com>
2023-12-08 21:48:26 +0800

    LATEST DB

--- celestia.sql
+++ celestia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 03:37 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 08, 2023 at 02:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,18 @@ CREATE TABLE `bedtype` (
   `bedtype_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `bedtype`
+--
+
+INSERT INTO `bedtype` (`bedtype_id`, `bedtype_name`) VALUES
+(1, '1 Single'),
+(2, '1 Queen'),
+(3, '1 King'),
+(4, '1 King, 1 Twin'),
+(5, '1 Queen, 1 Twin'),
+(6, 'King Luxurious Bedding');
+
 -- --------------------------------------------------------
 
 --
@@ -40,12 +52,53 @@ CREATE TABLE `bedtype` (
 
 CREATE TABLE `client` (
   `cid` int(11) NOT NULL,
-  `c_fname` int(11) NOT NULL,
-  `c_lname` int(11) NOT NULL,
-  `c_email` int(11) NOT NULL,
-  `c_phone` int(11) NOT NULL,
-  `c_city` int(11) NOT NULL
+  `c_fname` varchar(50) NOT NULL,
+  `c_lname` varchar(50) NOT NULL,
+  `c_email` varchar(50) NOT NULL,
+  `c_phone` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`cid`, `c_fname`, `c_lname`, `c_email`, `c_phone`) VALUES
+(5, 'Tzuyu', 'Chou', 'thinkaboutzu@gmail.com', '1237567262'),
+(7, 'Kc', 'pingol', 'pingol.kryss@ue.edu.ph', '0987845734'),
+(8, 'Chaewon', 'Kim', 'kfk.@gmail.com', '12345678901'),
+(9, 'Sana', 'Minatozaki', 'sm.Gmail.com', '12352567890');
+
+--
+-- Triggers `client`
+--
+DELIMITER $$
+CREATE TRIGGER `delete_reservation` AFTER DELETE ON `client` FOR EACH ROW BEGIN
+    DELETE FROM reserve WHERE clientID = OLD.cid;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mainadmin`
+--
+
+CREATE TABLE `mainadmin` (
+  `id` int(3) NOT NULL,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mainadmin`
+--
+
+INSERT INTO `mainadmin` (`id`, `fname`, `lname`, `gender`, `username`, `password`) VALUES
+(1, 'Kyle', 'Ngo', 'Male', 'admin_kyle', '123');
 
 -- --------------------------------------------------------
 
@@ -56,10 +109,47 @@ CREATE TABLE `client` (
 CREATE TABLE `rdetails` (
   `rid` int(11) NOT NULL,
   `rtypeid` int(11) NOT NULL,
+  `rname` varchar(100) NOT NULL,
   `btype_id` int(11) NOT NULL,
   `occupancy` int(11) NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rdetails`
+--
+
+INSERT INTO `rdetails` (`rid`, `rtypeid`, `rname`, `btype_id`, `occupancy`, `price`) VALUES
+(1, 1, 'Standard 1', 1, 1, 1000),
+(2, 1, 'Standard 2', 2, 2, 2500),
+(3, 1, 'Standard 3', 3, 2, 3000),
+(4, 2, 'Suite 1', 3, 4, 5555),
+(5, 4, 'S2', 6, 4, 8000),
+(10, 1, 'Standard 4', 1, 1, 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserve`
+--
+
+CREATE TABLE `reserve` (
+  `id` int(11) NOT NULL,
+  `clientID` int(11) NOT NULL,
+  `roomID` int(11) NOT NULL,
+  `chIn` date NOT NULL,
+  `chOut` date NOT NULL,
+  `Confirmation` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reserve`
+--
+
+INSERT INTO `reserve` (`id`, `clientID`, `roomID`, `chIn`, `chOut`, `Confirmation`) VALUES
+(13, 8, 5, '2023-12-08', '2023-12-10', 'yes'),
+(17, 7, 4, '2023-12-08', '2023-12-12', 'yes'),
+(19, 5, 2, '2023-12-08', '2023-12-10', 'yes');
 
 -- --------------------------------------------------------
 
@@ -71,6 +161,17 @@ CREATE TABLE `roomtype` (
   `rtypeid` int(3) NOT NULL,
   `rtype_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roomtype`
+--
+
+INSERT INTO `roomtype` (`rtypeid`, `rtype_name`) VALUES
+(1, 'Standard'),
+(2, 'Suite'),
+(3, 'Deluxe'),
+(4, 'Family Room'),
+(5, 'Penthouse');
 
 -- --------------------------------------------------------
 
@@ -86,6 +187,13 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `fname`, `lname`, `gender`, `username`, `password`) VALUES
+(2, 'Chaewon', 'Kim', 'Femal', 'ChaeChae', '123');
 
 --
 -- Indexes for dumped tables
@@ -104,10 +212,24 @@ ALTER TABLE `client`
   ADD PRIMARY KEY (`cid`);
 
 --
+-- Indexes for table `mainadmin`
+--
+ALTER TABLE `mainadmin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `rdetails`
 --
 ALTER TABLE `rdetails`
-  ADD PRIMARY KEY (`rid`);
+  ADD PRIMARY KEY (`rid`),
+  ADD UNIQUE KEY `rname` (`rname`);
+
+--
+-- Indexes for table `reserve`
+--
+ALTER TABLE `reserve`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roomID` (`roomID`);
 
 --
 -- Indexes for table `roomtype`
@@ -129,31 +251,43 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bedtype`
 --
 ALTER TABLE `bedtype`
-  MODIFY `bedtype_id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `bedtype_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `mainadmin`
+--
+ALTER TABLE `mainadmin`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `rdetails`
 --
 ALTER TABLE `rdetails`
-  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `reserve`
+--
+ALTER TABLE `reserve`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `roomtype`
 --
 ALTER TABLE `roomtype`
-  MODIFY `rtypeid` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `rtypeid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
